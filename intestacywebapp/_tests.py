9ae4fd8227c67d09aa12d259134db21616dff7cc
Mezzomaniac <Mezzomaniac@users.noreply.github.com>
2020-04-12 09:50:20 +0800

    tests fixed typos

--- intestacywebapp/_tests.py
+++ intestacywebapp/_tests.py
@@ -8,6 +8,6 @@ estate.spouse = Relative('Spouse', 'Wifey')
 estate.defactos = [DeFacto(name='Angela', length=5), DeFacto(name='Pamela', length=2), DeFacto(name='Sandra', length=0), DeFacto(name='Rita', length=2)]
 estate.parents = [Relative('Parent', 'Mum'), Relative('Parent', 'Dad')]
 estate.siblings = [DescendibleRelative('Surviving Sibling 1', 'Little Bro'), DescendibleRelative('Surviving Sibling 2', 'Little Sis')]
-estate.siblings.append(DescendibleRelative('Non-surviving Sibling 1', 'Big Bro', False, [Relative('Grandchild', 'Dog'), Relative('Grandchild', 'Cat'), Relative('Grandchild', 'Fish')]))
-estate.siblings.append(DescendibleRelative('Non-surviving Sibling 2', 'Big Sis', False, [Relative('Grandchild', 'Monkey'), Relative('Grandchild', 'Dolphin'), Relative('Grandchild', 'Lion')]))
+estate.siblings.append(DescendibleRelative('Non-surviving Sibling 1', 'Big Bro', False, [Relative('Niece', 'Dog'), Relative('Nephew', 'Cat'), Relative('Niece', 'Fish')]))
+estate.siblings.append(DescendibleRelative('Non-surviving Sibling 2', 'Big Sis', False, [Relative('Nephew', 'Monkey'), Relative('Niece', 'Dolphin'), Relative('Nephew', 'Lion')]))
 estate.distribute()
