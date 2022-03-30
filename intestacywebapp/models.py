639367c81fe3cbd80a70fe0f570b56bd9c8d5dfd
Mezzomaniac <themezj@hotmail.com>
2022-03-30 22:59:03 +0800

    models.py fixed specified items selection

--- intestacywebapp/models.py
+++ intestacywebapp/models.py
@@ -21,10 +21,10 @@ class Relative(db.Model):
     length = db.Column(db.Numeric)
     share = db.Column(db.Numeric)
     notes = db.Column(db.String)
-    
+
     def __repr__(self):
         return f'<{self.relation}>'
-    
+
     def __str__(self):
         return self.name or f'{ordinal_fmt(self.id)} {self.relation}'"""
 
@@ -50,10 +50,10 @@ class Relative:
         self.fraction = Fraction(0)
         self.share = Decimal('0.00')
         self.notes = []
-    
+
     def __repr__(self):
         return f'{self.__class__.__name__}({self.relationship}, {self.name}, survived={self.survived})'
-    
+
     @property
     def percentage(self):
         decimal = Decimal(self.fraction.numerator) / Decimal(self.fraction.denominator) * 100
@@ -67,7 +67,7 @@ class DescendibleRelative(Relative):
     def __init__(self, relationship:str, name:str='', survived=True, issue:list=[]):
         super().__init__(relationship, name, survived)
         self.issue = issue
-    
+
     def __repr__(self):
         return super().__repr__()[:-1] + f', issue={self.issue})'
 
@@ -82,12 +82,12 @@ class DeFacto(Relative):
         return super().__repr__()[:-1] + f', length={self.length})'
 
 class Estate:
-    
+
     def __init__(self, deathdate:datetime.date, value:Decimal):
         self.deathdate = deathdate
         self.value = Decimal(value)
         self.set_specified_items()
-        
+
         self.spouse = None
         self.defactos = []
         self.issue = []
@@ -96,23 +96,23 @@ class Estate:
         self.grandparents = []
         self.auntuncles = []
         self.crown = None
-    
+
     @property
     def eligible_spouse(self):
         return getattr(self.spouse, 'survived', False) and not any(defacto.length >= 5 for defacto in self.defactos)
-    
+
     @property
     def eligible_defactos(self):
         return sum(defacto.length >= 2 for defacto in self.defactos)
-    
+
     @property
     def eligible_partner(self):
         return self.eligible_spouse or bool(self.eligible_defactos)
-    
+
     @property
     def eligible_issue(self):
         return sum(child.survived or bool(child.issue) for child in self.issue)
-    
+
     @property
     def eligible_siblings(self):
         return sum(sibling.survived or bool(sibling.issue) for sibling in self.siblings)
@@ -120,7 +120,7 @@ class Estate:
     @property
     def eligible_auntuncles(self):
         return sum(auntuncle.survived or bool(auntuncle.issue) for auntuncle in self.auntuncles)
-    
+
     def __iter__(self):
         if self.spouse:
             yield self.spouse
@@ -131,7 +131,7 @@ class Estate:
             yield self.crown
 
     def set_specified_items(self):
-        for date, amounts in sorted(data.SPECIFIED_ITEMS.items()):
+        for date, amounts in reversed(sorted(data.SPECIFIED_ITEMS.items())):
             if self.deathdate >= date:
                 self.specified_items = {item.upper(): Decimal(amount) for  item, amount in amounts.items()}
                 for item, amount in self.specified_items.items():
@@ -141,7 +141,7 @@ class Estate:
 
     def to_json(self):
         return json.dumps(self, cls=MyEncoder)
-    
+
     def distribute(self):
         if self.eligible_partner and self.eligible_issue:
             self.item2()
@@ -163,16 +163,16 @@ class Estate:
             self.item10()
         else:
             self.item11()
-    
+
     def item2(self):
         fixed = Decimal('0.00')
         interest = Decimal('0.00')
         partner_fraction = Fraction(1)
         issue_fraction = Fraction(0)
-        
+
         partner_share = min(self.value, self.ITEM_2)
         balance = self.value - partner_share
-        
+
         if balance:
             fixed = self.ITEM_2
             interest = fixed * (datetime.date.today() - self.deathdate).days / 365 * Decimal('0.05')
@@ -188,15 +188,15 @@ class Estate:
 
         self.distribute_to_partners(partner_share, partner_fraction, fixed, interest)
         self.distribute_to_descendible_relatives('issue', balance, issue_fraction)
-        
+
     def item3(self):
         partner_fixed = Decimal('0.00')
         interest = Decimal('0.00')
         partner_fraction = Fraction(1)
-        
+
         partner_share = min(self.value, self.ITEM_3A_AND_B)
         balance = self.value - partner_share
-        
+
         if balance:
             partner_fixed = self.ITEM_3A_AND_B
             interest = partner_fixed * (datetime.date.today() - self.deathdate).days / 365 * Decimal('0.05')
@@ -209,10 +209,10 @@ class Estate:
             balance /= 2
 
         self.distribute_to_partners(partner_share, partner_fraction, partner_fixed, interest)
-        
+
         if not balance:
             return
-        
+
         if self.parents:
             parent_fixed = Decimal('0.00')
             parent_fraction = Fraction(1, 2)
@@ -225,13 +225,13 @@ class Estate:
                 siblings_fraction = Fraction(1, 4)
                 parent_share += balance / 2
                 balance /= 2
-                
+
             parents = len(self.parents)
             for parent in self.parents:
                 parent.share = parent_share / parents
                 parent.fraction = parent_fraction / parents
                 parent.fixed = parent_fixed / parents
-        
+
         else:
             siblings_fraction = Fraction(1, 2)
         if self.eligible_siblings:
@@ -239,18 +239,18 @@ class Estate:
 
     def item4(self):
         self.distribute_to_partners(self.value)
-    
+
     def item5(self):
         self.distribute_to_descendible_relatives('issue', self.value)
-    
+
     def item6(self):
         fixed = Decimal('0.00')
         parent_fraction = Fraction(1)
         siblings_fraction = Fraction(0)
-        
+
         parent_share = min(self.value, self.ITEM_6)
         balance = self.value - parent_share
-        
+
         if balance:
             fixed = self.ITEM_6
             parent_fraction = Fraction(1, 2)
@@ -270,27 +270,27 @@ class Estate:
         for parent in self.parents:
             parent.share = self.value / parents
             parent.fraction = Fraction(1) / parents
-    
+
     def item8(self):
         self.distribute_to_descendible_relatives('siblings', self.value)
-    
+
     def item9(self):
         grandparents = len(self.grandparents)
         for grandparent in self.grandparents:
             grandparent.share = self.value / grandparents
             grandparent.fraction = Fraction(1) / grandparents
-    
+
     def item10(self):
         self.distribute_to_descendible_relatives('auntuncles', self.value)
-    
+
     def item11(self):
         self.crown = Relative('The Crown')
         self.crown.share = self.value
         self.crown.fraction = Fraction(1)
-    
+
     def distribute_to_partners(self, share, fraction=Fraction(1), fixed=Decimal('0.00'), interest=Decimal('0.00')):
         portions = (self.eligible_spouse + bool(self.eligible_defactos))
-        notes = ['Plus chattels.']
+        notes = ['Plus household chattels.']
         if fraction < 1 or portions > 1 or self.eligible_defactos > 1:
             notes.append('May choose to receive home being lived in at date of death instead of monetary equivalent.')
         share /= portions
@@ -310,12 +310,12 @@ class Estate:
                 defacto.fixed = fixed / self.eligible_defactos
                 defacto.interest = interest / self.eligible_defactos
                 defacto.notes = notes
-        
+
     def distribute_to_descendible_relatives(self, relatives, share, fraction=Fraction(1)):
         portions = getattr(self, f'eligible_{relatives}')
         fraction /= portions
         share /= portions
-        
+
         for relative in getattr(self, relatives):
             if relative.survived:
                 relative.fraction = fraction
