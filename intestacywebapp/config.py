aad002725bbb9c6ea93ddbdf268fe415ce54e132
Mezzomaniac <Mezzomaniac@users.noreply.github.com>
2022-04-01 22:50:05 +0800

    incremented version no.

--- intestacywebapp/config.py
+++ intestacywebapp/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(16)
-    VERSION = '1.0.0'
+    VERSION = '1.1.0'
     
     #TESTING = True
     #SEND_FILE_MAX_AGE_DEFAULT = 0  # For development only
