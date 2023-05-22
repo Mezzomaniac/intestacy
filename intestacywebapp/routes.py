ca01cbc59cc9b70f98678add10af08332656b287
Mez PythonAnywhere <themezj@hotmail.com>
2023-05-22 06:10:19 +0000

    fixed repo path in webhook

--- intestacywebapp/routes.py
+++ intestacywebapp/routes.py
@@ -89,6 +89,6 @@ def distribution():
 def webhook():
     if request.method != 'POST':
         return 'Wrong event type', 400
-    repo = git.Repo('https://github.com/Mezzomaniac/intestacy')
+    repo = git.Repo('/home/themezj/intestacy')
     repo.remotes.origin.pull()
     return 'Updated PythonAnywhere successfully'
