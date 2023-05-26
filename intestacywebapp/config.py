1992adf3558d8acfc64be218d6f8b46c1aa4c4df
Mezzomaniac <Mezzomaniac@users.noreply.github.com>
2023-05-26 13:39:20 +0800

    added new update message

--- intestacywebapp/config.py
+++ intestacywebapp/config.py
@@ -3,7 +3,7 @@ import secrets
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', secrets.token_hex())
-    VERSION = '1.2.5'
+    VERSION = '1.3.0'
 
     #TESTING = True
     #SEND_FILE_MAX_AGE_DEFAULT = 0  # For development only
